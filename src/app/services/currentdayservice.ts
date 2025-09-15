5e6ddc09f3e508ef0e0a62d81233fedb222f0929
Richel N <rnssaketo@proton.me>
2025-09-15 12:46:57 +0000

    Change time calculation

--- src/app/services/currentdayservice.ts
+++ src/app/services/currentdayservice.ts
@@ -10,9 +10,37 @@ export class Currentdayservice {
   date = new Date()
 
   // currentNextClass = this.schoolDays()[this.currentDay-1].classesList
-  currentNextClass = this.date
+  currentNextClass = this.nextClassCalculator(this.currentDay, this.date)
+
+  timeStringSplitter(timestring: string){
+    const [h,m,restoftime] = timestring.split(":")
+    const [s, indicator] = restoftime.split(" ")
+    return [h,m,s,indicator]
+  }
 
   nextClassCalculator(day: number, date: Date){
-    const classlist = this.schoolDays()[day-1].classesList
+    const dayObject = this.schoolDays()[day-1]
+    const classlist = dayObject.classesList
+    const time = date.toLocaleTimeString()
+
+    const [, minutes, seconds] = this.timeStringSplitter(time)
+    const hours = +this.timeStringSplitter(time)[0]+12
+    const hoursminutes = hours+'||'+minutes
+    
+   
+
+    if (dayObject.halfDay) {
+        if (hours >= 0 && hours <= 9) {
+          return classlist[0]
+      }
+      if (hours >= 9) {
+          return classlist[1]
+      }
+      if (hours >= 10) {
+          return classlist[2]
+      }
+    }
+
+    return `${hours}:${minutes}:${seconds}`
   }
 }
